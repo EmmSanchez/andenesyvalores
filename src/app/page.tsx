304e5b51d8727231815f932d8d41bfa5d02cc4d5
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2025-09-25 01:28:36 -0600

    Empezando formulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -303,22 +303,99 @@ export default function Home() {
             </p>
             <Link
               href={"/servicios"}
-              className="w-[175px] h-[45px] text-[22px] leading-[120%] tracking-[-2%] px-[15px] py-3 text-center font-semibold text-button-blue bg-button-active cut-corner active:bg-icons-orange"
+              className="w-[263px] h-[45px] text-[22px] leading-[120%] tracking-[-2%] px-[15px] py-3 text-center font-semibold text-button-blue bg-button-active cut-corner active:bg-icons-orange"
             >
-              Ver más
+              Ver recorrido 360°
             </Link>
           </div>
         </div>
       </section>
 
-      <section>
-        <h1>Hola</h1>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      {/* Partners */}
+      <section className="w-[1280px] h-[359px] flex flex-col justify-center items-center gap-[30px] px-[80px] py-[50px] text-center">
+        <h2 className="flex gap-[10px] text-[64px] text-title-3 leading-[95%] tracking-wide">
+          Colaboraciones y{" "}
+          <span className="text-title-4 font-semibold">Aliados</span>
+        </h2>
+        <p className="w-[1120px] text-[22px] text-black">
+          En Andenes y Valores creemos en el poder de las alianzas. Por eso
+          mantenemos una comunicación constante con nuestros clientes y
+          colaboradores para generar oportunidades de valor conjunto.
+        </p>
+
+        <div className="flex justify-center items-center w-[1120px] h-[84px] gap-[24px]">
+          <div className="h-[84px] flex justify-center items-center p-4">
+            <Image
+              src={"/logos/partners/logo-1.svg"}
+              width={75}
+              height={64}
+              alt="Logo partner 1"
+              className="h-[64px] w-[75px]"
+            />
+          </div>
+          <div className="h-[84px] flex justify-center items-center p-4">
+            <Image
+              src={"/logos/partners/logo-2.svg"}
+              width={220}
+              height={64}
+              alt="Logo partner 2"
+              className="w-[220px] h-[64px]"
+            />
+          </div>
+          <div className="h-[84px] flex justify-center items-center p-4">
+            <Image
+              src={"/logos/partners/logo-3.svg"}
+              width={53}
+              height={60}
+              alt="Logo partner 3"
+              className="w-[63px] h-[63px]"
+            />
+          </div>
+          <div className="h-[84px] flex justify-center items-center p-4">
+            <Image
+              src={"/logos/partners/logo-4.svg"}
+              width={205}
+              height={64}
+              alt="Logo partner 4"
+              className="w-[205px] h-[64px]"
+            />
+          </div>
+          <div className="h-[84px] flex justify-center items-center p-4">
+            <Image
+              src={"/logos/partners/logo-5.svg"}
+              width={146}
+              height={62}
+              alt="Logo partner 5"
+              className="w-[146px] h-[62px]"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Contactanos */}
+      <section className="w-full h-[715px] flex flex-col justify-center items-center bg-background-2">
+        <div className="w-[1280px] flex flex-col justify-center items-center gap-[30px] py-[50px]">
+          <h2 className="w-[1180px] h-[122px] flex flex-col text-[64px] text-center tracking-wide leading-[105%] text-title-2">
+            Tu historia merece ser contada{" "}
+            <span className="text-title-4 font-semibold">Contáctanos</span>
+          </h2>
+
+          {/* Formulario */}
+          <div className="w-[1280px] h-[463px] flex justify-center items-center gap-[15px]">
+            <div className="w-[456.2px] h-[453.52] box p-5">
+              <h3 className="w-full h-full pt-16 flex flex-col text-[64px] text-right font-bold tracking-wide leading-[105%] text-title-2">
+                <span className="text-title-4">JUNTOS</span>
+                CREAMOS
+                <span className="text-title-4">FUTUROS.</span>
+              </h3>
+            </div>
+
+            <form action="" className="bg-green-200 w-[747px] h-[463px]">
+              <label htmlFor="">Nombre</label>
+              <input type="text" className="w-full h-14 bg-background-1" />
+            </form>
+          </div>
+        </div>
       </section>
     </main>
   );
